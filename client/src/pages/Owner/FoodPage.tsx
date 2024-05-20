a8fe5cb54a591a1eee3257b78bffc6070e06405f
yugh <yughas83@gmail.com>
2024-05-20 13:02:22 +0530

    added responsive

--- client/src/pages/Owner/FoodPage.tsx
+++ client/src/pages/Owner/FoodPage.tsx
@@ -14,7 +14,7 @@ const FoodPage = () => {
     }, [trigger])
     
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {allFood?.map((item:any) => {
             return <FoodCard trigger={trigger} reload={reload} foodName={item.food} price={item.price}/>
         })}
